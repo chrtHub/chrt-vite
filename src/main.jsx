ec306be51598ae22d1e12e53533fa446280a1f50
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-01-31 17:06:38 -0600

    Auth0ProviderWithNavigate set up, need to build Guarded Route and test it out

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import {
-  // Outlet,
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
 
-import { Auth0Provider } from "@auth0/auth0-react";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 
 import Layout from "./Components/Layout";
 
@@ -24,30 +23,21 @@ import "./index.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path="journal" element={<Journal />} />
-      <Route path="data" element={<Data />} />
-      <Route path="settings" element={<Settings />} />
-      <Route path="profile" element={<Profile />} />
-      <Route path="callback" element={<Callback />} />
+    <Route element={<Auth0ProviderWithNavigate />}>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="journal" element={<Journal />} />
+        <Route path="data" element={<Data />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="callback" element={<Callback />} />
+      </Route>
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-u4trvdw25pkfbgaq.us.auth0.com"
-      clientId="nJH6eXO0snTYdLQ2W9Zm6JndVwD4ActU" //-- Application: chrt-vite --//
-      authorizationParams={{
-        redirect_uri: `${window.location.origin}/callback`,
-        // audience: "https://dev-u4trvdw25pkfbgaq.us.auth0.com/api/v2/", //-- Auth0 Management API --//
-        // // "TODO"//-- chrt API --//
-        // scope: "read:current_user update:current_user_metadata",
-      }}
-    >
-      <RouterProvider router={router} />
-    </Auth0Provider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
