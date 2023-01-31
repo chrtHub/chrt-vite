f221f81b2017a2b1b263c14338d74571cd4d3b63
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-01-30 23:25:37 -0600

    implementing Auth0 - auth code + PKCE for SPA + API

--- src/auth/Auth0Profile.jsx
+++ src/auth/Auth0Profile.jsx
@@ -1,7 +1,47 @@
+import { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const [userMetadata, setUserMetadata] = useState(null);
+
+  const {
+    user,
+    isAuthenticated,
+    isLoading,
+    // getAccessTokenSilently
+  } = useAuth0();
+
+  // useEffect(() => {
+  //   const getUserMetadata = async () => {
+  //     const auth0_mgmt_api_domain = "dev-u4trvdw25pkfbgaq.us.auth0.com";
+
+  //     try {
+  //       const accessToken = await getAccessTokenSilently({
+  //         authorizationParams: {
+  //           audience: `https://${auth0_mgmt_api_domain}/api/v2/`,
+  //           scope: "read:current_user",
+  //         },
+  //       });
+
+  //       const metadataResponse = await fetch(
+  //         `https://${auth0_mgmt_api_domain}/api/v2/users/${user.sub}`,
+  //         {
+  //           headers: {
+  //             Authorization: `Bearer ${accessToken}`,
+  //           },
+  //         }
+  //       );
+
+  //       const { user_metadata } = await metadataResponse.json();
+
+  //       setUserMetadata(user_metadata);
+  //     } catch (e) {
+  //       console.log(e.message);
+  //     }
+  //   };
+
+  //   getUserMetadata();
+  // }, [getAccessTokenSilently, user?.sub]);
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -10,9 +50,15 @@ const Profile = () => {
   return (
     isAuthenticated && (
       <div>
-        <img src={user.picture} alt={user.name} />
         <h2>user.name: {user.name}</h2>
         <p>user.email: {user.email}</p>
+        <img src={user.picture} alt={user.name} />
+        {/* <h3>User Metadata</h3>
+         {userMetadata ? (
+          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+        ) : (
+          "No user metadata defined"
+        )} */}
       </div>
     )
   );
--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,14 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
       domain="dev-u4trvdw25pkfbgaq.us.auth0.com"
-      clientId="nJH6eXO0snTYdLQ2W9Zm6JndVwD4ActU"
-      authorizationParams={{ redirect_uri: window.location.origin }}
+      clientId="nJH6eXO0snTYdLQ2W9Zm6JndVwD4ActU" //-- Application: chrt-vite --//
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        // audience:
+        // "https://dev-u4trvdw25pkfbgaq.us.auth0.com/api/v2/", //-- Auth0 Management API --//
+        // // "TODO"//-- chrt API --//
+        // scope: "read:current_user update:current_user_metadata",
+      }}
     >
       <App />
     </Auth0Provider>
