b7133224b76965e5dac2e18e8ccdfc1fe2d92955
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-02-01 20:59:52 -0600

    fiddled with layout grays but I think it’s the same as original

--- src/App/Layout/Layout.jsx
+++ src/App/Layout/Layout.jsx
@@ -198,7 +198,7 @@ export default function Layout() {
           <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow">
             <button
               type="button"
-              className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
+              className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500 md:hidden"
               onClick={() => setSidebarOpen(true)}
             >
               <span className="sr-only">Open sidebar</span>
@@ -231,7 +231,7 @@ export default function Layout() {
               <div className="ml-4 flex items-center md:ml-6">
                 {/* <button
                   type="button"
-                  className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
                 >
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
@@ -240,7 +240,7 @@ export default function Layout() {
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2">
                       <span className="sr-only">Open user menu</span>
                       <UserCircleIcon className="h-8 w-8 rounded-full" />
                       {/* <img
