27e4aab80c7ef467590a6b72f5528451597bf639
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-02-05 01:02:46 -0600

    implementing RBAC for Express API using Auth0

--- src/App/JournalService/Journal.jsx
+++ src/App/JournalService/Journal.jsx
@@ -60,7 +60,6 @@ export default function Journal() {
 
   return (
     <>
-      {/*  */}
       <div className="m-2 divide-y divide-zinc-200 overflow-hidden rounded-lg bg-white text-black shadow dark:divide-zinc-600 dark:bg-zinc-800 dark:text-white">
         <div className="px-4 py-5 sm:p-6">
           {journalData ? (
