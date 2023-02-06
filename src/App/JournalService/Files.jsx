c95467fecd062fbae00dab79a6f1c487d72c4f04
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-02-06 01:45:07 -0600

    added /files route

--- src/App/JournalService/Files.jsx
+++ src/App/JournalService/Files.jsx
@@ -1,3 +1,18 @@
+//-- react, react-router-dom, Auth0 --//
+
+//-- JSX Components --//
+
+//-- NPM Components --//
+
+//-- Icons --//
+
+//-- NPM Functions --//
+
+//-- Utility Functions --//
+
+//-- Data Objects --//
+
+//-- ***** ***** ***** Exported Component ***** ***** ***** --//
 export default function Journal() {
   return <div>files</div>;
 }
