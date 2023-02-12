144ca1451b04a62628c9c56ff24082c5eda9baec
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-02-12 00:15:47 -0600

    basic setup done, need to do styling and set max width and margins

--- src/Layout/AppLayout.jsx
+++ src/Layout/AppLayout.jsx
@@ -449,22 +449,15 @@ export default function Example() {
           </div>
           {/* START OF HAMBURGER BUTTON + SEARCH BAR + PROFILE PICTURE + DROPDOWN MENU */}
 
+          {/* START OF MAIN */}
           <main className="flex-1">
-            <div className="py-6">
-              <div className="px-4 sm:px-6 md:px-0">
-                <h1 className="text-2xl font-semibold text-gray-900">
-                  Dashboard
-                </h1>
-              </div>
-              <div className="px-4 sm:px-6 md:px-0">
-                {/* Replace with your content */}
-                <div className="py-4">
-                  <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
-                </div>
-                {/* /End replace */}
-              </div>
-            </div>
+            {/* <div className="py-6"> */}
+            {/* <div className="px-4 sm:px-6 md:px-0"> */}
+            <Outlet />
+            {/* </div> */}
+            {/* </div> */}
           </main>
+          {/* END OF MAIN */}
         </div>
       </div>
       {/* END OF RHS */}
