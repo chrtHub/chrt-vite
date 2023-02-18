6b3d3a821b51158fb1612db8a4fa686b5b926c4f
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-02-18 01:35:16 -0600

    animated title on landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,23 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        text: "text 15s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
