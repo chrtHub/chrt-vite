7fabc5d9901cb40b1e352855ea80bec3f4b9f7e1
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-02-24 18:08:13 -0600

    updated pulse to custom pulseDeep

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ export default function App() {
     let timeout;
     timeout = setTimeout(() => {
       setShowLoading(false);
-    }, 300);
+    }, 1000);
 
     return () => clearTimeout(timeout);
   }, [isLoading]);
