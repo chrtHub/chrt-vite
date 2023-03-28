ad2ff6864bfb7d0901e70144f9611814f2a6db85
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-03-27 19:46:54 -0500

    working on chatson

--- src/App/GPT/chatson/chatson.tsx
+++ src/App/GPT/chatson/chatson.tsx
@@ -1,39 +1,85 @@
 import { Dispatch, SetStateAction } from "react";
-import A from "./A.json";
+import { v4 as uuidv4 } from "uuid";
+import { getUnixTime } from "date-fns";
 
-/**
- * Creates a new chat object
- *
- * @param chatson_format indicate chatson format version
- * @returns chatson object intialized with basic information
- */
-export enum Chatson_Format {
-  "2023-03-26-A" = "2023-03-26-A",
-}
-export function create_chat(chatson_format: Chatson_Format): Object {
-  // get a fresh version of the specified format of chatson object
-  // fill in the basic information
+import { IChatsonObject, IChatsonMessage } from "./types";
 
-  return A;
-}
+//-- Utility Function --//
+const timestamp = (): string => {
+  return getUnixTime(new Date()).toString();
+};
 
 /**
  * Causes an LLM API call after adding a propmt to a chatson object
  *
  * @param prompt user input to be added to the chat history and sent to the LLM
- * @param chatson_object_state chatson chat object stored as a React state value
- * @param state_setter useState setter function used for updating the chatson object
- * @returns void
+ * @param user_ids array of user ids. current user should be at index 0.
+ * @param chatson_object if null, a new chat is created. otherwise, the prompt is appended to the chatson_object
+ * @returns IChatsonObject updated with the new prompt
  */
-export function send_prompt(
+export function add_prompt(
   prompt: string,
-  chatson_object_state: string,
-  state_setter: Dispatch<SetStateAction<String>>
+  user_ids: string[],
+  chatson_object: IChatsonObject | null
 ) {
+  //-- If no chat object, create a new chat object --//
+  if (!chatson_object) {
+    let newChatObject: IChatsonObject = version_A;
+
+    newChatObject.metadata["single-or-multi-user"] = "single";
+    newChatObject.metadata.user_ids = user_ids;
+    newChatObject.metadata.chat_uuid = uuidv4();
+    newChatObject.metadata.creation_timestamp_immutable = timestamp();
+    newChatObject.metadata.reference_timestamp_mutable = timestamp();
+
+    console.log(JSON.stringify(newChatObject, null, 2)); // DEV
+  }
+
   // TODO
+  let new_message: IChatsonMessage = {
+    role: "user",
+    author: user_ids[0],
+    timestamp: timestamp(),
+    message_uuid: uuidv4(),
+    content: prompt,
+  };
   // add prompt to chatson object
+  // chatson_object.linear_message_history.push(new_message);
   // extract appropriate messages from chatson object to be sent to the LLM API
   // // e.g. system message, previous chat messages up to model's token limit
   // make an API call
   // when the response is fully received, it's added to the chatson object
 }
+
+//-- Chatson Templates --//
+export const version_A: IChatsonObject = {
+  chatson_version: "A",
+  notes: {
+    metadata: "Basic info and stats about this chat object",
+    linear_message_history:
+      "Mutable message history used as source of truth for (1) sending requests to the LLM, (2) rendering a chat on screen to a user. Note - the system message is immutable.",
+    api_call_history:
+      "An append-only log of API calls used to track token consumption and for general reference.",
+  },
+  metadata: {
+    "single-or-multi-user": "",
+    user_ids: [],
+    chat_uuid: "",
+    creation_timestamp_immutable: "",
+    reference_timestamp_mutable: "",
+    user_tags: [],
+    chrt_tags: [],
+    opensearch_tags: [],
+  },
+  linear_message_history: [
+    {
+      role: "system",
+      author: "chrt",
+      timestamp: "",
+      message_uuid: "",
+      content:
+        "Your name is ChrtGPT. Refer to yourself as ChrtGPT. You are ChrtGPT, a helpful assistant that helps power a day trading performance journal. You sometimes make jokes and say silly things on purpose.",
+    },
+  ],
+  api_call_history: [],
+};
