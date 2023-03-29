d04b8dd9ca8a5c0908dd11cacdf9365af83c139a
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-03-29 12:52:59 -0500

    trying to add top level await plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
 import Inspect from "vite-plugin-inspect";
+import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 //-- https://vitejs.dev/config/ --//
@@ -11,6 +12,14 @@ export default defineConfig({
     [visualizer() as PluginOption],
     [Inspect() as PluginOption],
     [wasm() as PluginOption],
+    [
+      topLevelAwait({
+        // The export name of top-level await promise for each chunk module
+        promiseExportName: "__tla",
+        // The function to generate import names of top-level await promise in each chunk module
+        promiseImportName: (i) => `__tla_${i}`,
+      }) as PluginOption,
+    ],
   ],
   resolve: {
     preserveSymlinks: true,
