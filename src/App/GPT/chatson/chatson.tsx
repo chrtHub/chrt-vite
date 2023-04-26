09ccbb1059e2b14764e8b0c8e51ef83772ed95e3
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-04-26 13:49:15 -0500

    implementing tree-based chat

--- src/App/GPT/chatson/chatson.tsx
+++ src/App/GPT/chatson/chatson.tsx
@@ -70,10 +70,37 @@ export async function send_message(
       headers: headers,
       body: JSON.stringify(request_body),
       async onopen(res) {
-        //
+        // TODO
+        // if new conversation
+        // build conversation object, store in ChatContext?
+        // if continuing conversation
       },
       onmessage(event) {
-        //
+        //-- Error --//
+        if (event.id && event.id === "error") {
+          console.log("error", event.data); // DEV
+          // TODO - implement error handling
+        }
+        //-- Conversation object (IConversation) --//
+        else if (event.id && event.id === "conversation") {
+          let res: IConversation = JSON.parse(event.data);
+          setConversation(res);
+        }
+        //-- Completion object (IMessage) --//
+        else if (event.id && event.id === "completion") {
+          let res: IMessage = JSON.parse(event.data);
+          // TODO - update completion
+        }
+        //-- API Req/Res Metadata (IAPIReqResMetadata)
+        else if (event.id && event.id === "api_req_res_metadata") {
+          let res: IAPIReqResMetadata = JSON.parse(event.data);
+          // TODO - update api_req_res_metadata
+        }
+        //-- SSE completion content chunks --//
+        else {
+          const uriDecodedData = decodeURIComponent(event.data);
+          // TODO - update completion.content
+        }
       },
       onclose() {
         console.log("Connection closed by the server"); // DEV
