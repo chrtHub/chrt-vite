29e7d12668c91707f438dd166190fdbd96ad65c6
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-05-12 01:01:19 -0500

    change_branch finds final first-born descendant of new sibling node

--- src/App/GPT/chatson/chatson.tsx
+++ src/App/GPT/chatson/chatson.tsx
@@ -500,20 +500,44 @@ export async function send_message(
 /**
  * Change branch - rebuilds rowArray by updating the leaf node as a node from among the siblings of the current message
  *
- * @param new_leaf_node_id
+ * @param new_sibling_node_id
  * @param CC
  */
 export function change_branch(
-  new_leaf_node_id: string,
+  new_sibling_node_id: string,
   CC: IChatContext
 ): void {
   console.log("--- change branch ---"); // DEV
-  const leaf_node = nodeArray.find((node) => node._id === new_leaf_node_id);
-  console.log("leaf_node: ", leaf_node); // DEV
-  if (leaf_node) {
-    const rowArray = nodeArrayToRowArray(nodeArray, leaf_node);
+  const new_sibling_node = nodeArray.find(
+    (node) => node._id === new_sibling_node_id
+  );
+
+  if (new_sibling_node) {
+    let new_leaf_node: IMessageNode;
+
+    //-- If new sibling has no children, it's the leaf node --//
+    if (new_sibling_node.children_node_ids.length === 0) {
+      new_leaf_node = new_sibling_node;
+    }
+    //-- Else recursively find the first-born descendants --//
+    else {
+      new_leaf_node = finalFirstborn(new_sibling_node);
+    }
+    //-- Use new_leaf_node to build new rowArray and set it in state --//
+    const rowArray = nodeArrayToRowArray(nodeArray, new_leaf_node);
     CC.setRowArray(rowArray);
   }
+}
+
+function finalFirstborn(node: IMessageNode): IMessageNode {
+  while (node.children_node_ids.length > 0) {
+    let next_node_id = node.children_node_ids[0]; //-- Firstborn --//
+    let next_node = nodeArray.find((node) => node._id === next_node_id);
+    if (next_node) {
+      node = next_node;
+    }
+  }
+  return node;
 }
 
 /**
