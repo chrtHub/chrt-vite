db92ae69c985774be28cafaba161dc62164eccca
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-05-17 18:08:43 -0500

    changed New Conversation button from zinc to green and made it rounder

--- src/App/GPT/Sidebar/Buttons/ConversationButton.tsx
+++ src/App/GPT/Sidebar/Buttons/ConversationButton.tsx
@@ -16,13 +16,16 @@ export const ConversationButton: React.FC<ConversationButtonProps> = ({
       type="button"
       className={classNames(
         //-- Normal --//
-        "mb-2 mt-1 inline-flex w-full items-center justify-center gap-x-1.5 rounded-md border-zinc-400 bg-zinc-300 px-2.5 py-1.5 text-sm font-semibold text-zinc-800 shadow-sm",
+        "mb-2 mt-1 inline-flex w-full items-center justify-center gap-x-1.5 rounded-2xl px-2.5 py-1.5 text-sm font-semibold shadow-sm",
+        //----//
+        "bg-green-300 text-green-800",
         //-- Hover --//
-        "hover:border-zinc-400 hover:bg-zinc-400 hover:text-zinc-50",
+        "hover:bg-green-400 hover:text-green-900",
         //-- Focus --//
         "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-600",
         //-- Dark --//
-        "dark:bg-zinc-800 dark:text-zinc-100 dark:hover:bg-zinc-600"
+        "dark:bg-green-900 dark:text-green-200 dark:hover:bg-green-700 dark:hover:text-green-100"
+        // "dark:bg-zinc-800 dark:text-zinc-100 dark:hover:bg-zinc-600"
       )}
       onClick={onClick}
     >
