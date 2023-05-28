4f1107f68de88762ca009c27a13aabcfbd3de000
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-05-28 16:00:43 -0500

    removed vite wasm plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import mdx from "@mdx-js/rollup";
 import { visualizer } from "rollup-plugin-visualizer";
 import Inspect from "vite-plugin-inspect";
-import wasm from "vite-plugin-wasm";
 
 //-- https://vitejs.dev/config/ --//
 export default defineConfig({
@@ -12,7 +11,6 @@ export default defineConfig({
     [mdx() as PluginOption],
     [visualizer() as PluginOption],
     [Inspect() as PluginOption],
-    [wasm() as PluginOption],
   ],
   resolve: {
     preserveSymlinks: true,
