4bdc79421fafa8827cb7cc85549eccada6eaef54
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-05-28 15:59:19 -0500

    removed tiktoken and vite top-level await plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import mdx from "@mdx-js/rollup";
 import { visualizer } from "rollup-plugin-visualizer";
 import Inspect from "vite-plugin-inspect";
-import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 //-- https://vitejs.dev/config/ --//
@@ -14,14 +13,6 @@ export default defineConfig({
     [visualizer() as PluginOption],
     [Inspect() as PluginOption],
     [wasm() as PluginOption],
-    [
-      topLevelAwait({
-        // The export name of top-level await promise for each chunk module
-        promiseExportName: "__tla",
-        // The function to generate import names of top-level await promise in each chunk module
-        promiseImportName: (i) => `__tla_${i}`,
-      }) as PluginOption,
-    ],
   ],
   resolve: {
     preserveSymlinks: true,
