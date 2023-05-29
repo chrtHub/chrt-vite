6572b5d952caf17640721477ac71fe2c7c45d033
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-05-29 01:00:33 -0500

    matching colors across CTA401Fallbacks

--- src/App/GPT/ChatRowAreaFallback.tsx
+++ src/App/GPT/ChatRowAreaFallback.tsx
@@ -1,5 +1,7 @@
 import { getErrorDetails } from "../../Errors/getErrorDetails";
 
+import { useNavigate } from "react-router-dom";
+
 export const ChatRowAreaFallback = ({ error }: { error: Error }) => {
   const {
     errorMessage,
@@ -10,25 +12,25 @@ export const ChatRowAreaFallback = ({ error }: { error: Error }) => {
   } = getErrorDetails(error);
   const is401Error = axiosHTTPStatus === "401";
 
+  let navigate = useNavigate();
+
   if (is401Error) {
     return (
       <div className="flex w-full flex-grow flex-row justify-center">
-        <div className="flex w-full max-w-prose flex-col justify-center rounded-lg bg-zinc-200 text-center dark:bg-zinc-800">
-          <h2 className="text-3xl font-bold tracking-tight text-zinc-900 dark:text-zinc-100">
+        <div className="flex w-full max-w-prose flex-col justify-center rounded-lg bg-zinc-300 text-center dark:bg-zinc-500">
+          <h2 className="text-3xl font-bold tracking-tight text-zinc-900 dark:text-zinc-50 lg:text-4xl">
             Start using ChrtGPT today
-            {/* <br /> */}
-            {/* today */}
           </h2>
-          <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-zinc-600 dark:text-zinc-200">
             Request free access to the preview release
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+            <button
+              onClick={() => navigate("/profile")}
+              className="text-md rounded-md bg-green-600 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
             >
               Get Access <span aria-hidden="true">→</span>
-            </a>
+            </button>
           </div>
         </div>
       </div>
