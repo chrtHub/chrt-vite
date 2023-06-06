4a3579eb91a1c97c00074938206767247d0f19c4
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-06-06 00:46:51 -0500

    updated /signin to redirect to home for authenticated users, will still redirect to login page for unknown users

--- src/Navigation/RedirectToSignIn.tsx
+++ src/Navigation/RedirectToSignIn.tsx
@@ -1,3 +1,16 @@
+//-- 2 scenarios: --//
+// If not signed in, redirect to login page via AuthGuard
+// If signed in, redirect to home page
+
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function RedirectToSignIn() {
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/");
+  }, []);
+
   return <></>;
 }
