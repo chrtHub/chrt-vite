5093093fce42ffc2201c1384c89da07eb78732bd
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-06-09 15:28:50 -0500

    working on logic for granting consent

--- src/App/Account/DataPrivacy/DataPrivacy.tsx
+++ src/App/Account/DataPrivacy/DataPrivacy.tsx
@@ -3,9 +3,22 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useAccountContext } from "../../../Context/AccountContext";
 import classNames from "../../../Util/classNames";
 
+import { ErrorBoundary, useErrorBoundary } from "react-error-boundary";
+import { axiosErrorToaster } from "../../../Errors/axiosErrorToaster";
+
+import axios, { AxiosError } from "axios";
+
+let VITE_ALB_BASE_URL: string | undefined = import.meta.env.VITE_ALB_BASE_URL;
+
+//-- Version effective dates --//
+const TERMS_VERSION_EFFECTIVE_DATE = "2023-06-09";
+const PRIVACY_VERSION_EFFECTIVE_DATE = "2023-06-09";
+const COOKIES_VERSION_EFFECTIVE_DATE = "2023-06-09";
+
 export default function DataPrivacy() {
   let AccountContext = useAccountContext();
   let { getAccessTokenSilently, user } = useAuth0();
+  const { showBoundary } = useErrorBoundary();
 
   const [termsChecked, setTermsChecked] = useState<boolean>(false);
   const [privacyChecked, setPrivacyChecked] = useState<boolean>(false);
@@ -23,26 +36,65 @@ export default function DataPrivacy() {
 
   interface doc {
     name: string;
+    versionEffectiveDate: string;
     href: string;
     setterFn: React.Dispatch<React.SetStateAction<boolean>>;
   }
   const docs: doc[] = [
     {
       name: "Terms of Service",
+      versionEffectiveDate: TERMS_VERSION_EFFECTIVE_DATE,
       href: "https://chrt.com/terms",
       setterFn: setTermsChecked,
     },
     {
       name: "Privacy Statement",
+      versionEffectiveDate: PRIVACY_VERSION_EFFECTIVE_DATE,
       href: "https://chrt.com/privacy",
       setterFn: setPrivacyChecked,
     },
     {
       name: "Cookies Policy",
+      versionEffectiveDate: COOKIES_VERSION_EFFECTIVE_DATE,
       href: "https://chrt.com/cookies",
       setterFn: setCookiesChecked,
     },
   ];
+
+  //-- Submit Clickwrap Handler --//
+  const submitClickwrapHandler = async () => {
+    console.log("clickwrap time");
+    try {
+      //-- Get access token from memory or request new token --//
+      let accessToken = await getAccessTokenSilently();
+
+      //-- Make POST request --//
+      let res = await axios.post(
+        `${VITE_ALB_BASE_URL}/legal/grant_consent`,
+        //-- Body Content --//
+        {
+          TERMS_VERSION_EFFECTIVE_DATE: TERMS_VERSION_EFFECTIVE_DATE,
+          PRIVACY_VERSION_EFFECTIVE_DATE: PRIVACY_VERSION_EFFECTIVE_DATE,
+          COOKIES_VERSION_EFFECTIVE_DATE: COOKIES_VERSION_EFFECTIVE_DATE,
+        },
+        {
+          headers: {
+            authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      console.log(res); // DEV
+      //----//
+    } catch (err) {
+      console.log(err);
+      // showBoundary(err)
+      if (err instanceof AxiosError) {
+        axiosErrorToaster(err, "");
+      }
+    }
+
+    // TODO
+  };
 
   return (
     <div className="max-w-lg rounded-lg bg-zinc-200 px-6 py-1 shadow">
@@ -63,7 +115,13 @@ export default function DataPrivacy() {
                   {doc.name}
                 </a>
               </p>
+
+              {/* NEW - version effective date*/}
+              <p className="text-sm text-zinc-600">
+                Version Effective Date: {doc.versionEffectiveDate}
+              </p>
             </div>
+
             {/* Checkbox */}
             <div className="flex h-6 w-10 items-center">
               <input
@@ -132,7 +190,7 @@ export default function DataPrivacy() {
         <button
           disabled={!allChecked}
           type="button"
-          onClick={() => console.log("clickwrap time")}
+          onClick={submitClickwrapHandler}
           className={classNames(
             "flex items-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
             !allChecked
