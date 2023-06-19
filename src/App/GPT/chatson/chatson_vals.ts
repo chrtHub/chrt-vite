222352ab25a7bd7dd09e4c0b946a2f72fb38eba7
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-06-19 18:51:42 -0500

    added gpt-3.5-16k

--- src/App/GPT/chatson/chatson_vals.ts
+++ src/App/GPT/chatson/chatson_vals.ts
@@ -1,9 +1,10 @@
-import { TokenLimit, ResponseBuffer } from "./chatson_types";
+import { TokenLimit, ResponseBuffer } from "./chatson_types.js";
 
 const BUFFER: number = 96;
 
 export const TOKEN_LIMITS: TokenLimit = {
   "gpt-3.5-turbo": 4096 - BUFFER,
+  "gpt-3.5-turbo-16k": 16384 - BUFFER,
   "gpt-4": 4096 - BUFFER,
   "gpt-4-32k": 0,
   claude: 0,
@@ -15,6 +16,7 @@ export const TOKEN_LIMITS: TokenLimit = {
 //-- Use to limit the total size of request messages so that at least RESPONSE_BUFFER tokens can be returned. The prompt itself can be up to TOKEN_LIMITS tokens, but historical messages won't be added RESPONSE_BUFFER of the model's limit. --//
 export const RESPONSE_BUFFERS: ResponseBuffer = {
   "gpt-3.5-turbo": 1096,
+  "gpt-3.5-turbo-16k": 1096,
   "gpt-4": 1096,
   "gpt-4-32k": 0,
   claude: 0,
