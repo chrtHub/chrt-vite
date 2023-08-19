6b2c60f64e832f800a67a2923c71f486cadf74ae
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-08-19 13:41:16 -0500

    updating the data privacy page

--- src/App/Account/DataPrivacy/DataPrivacy.tsx
+++ src/App/Account/DataPrivacy/DataPrivacy.tsx
@@ -29,10 +29,22 @@ export default function DataPrivacy() {
   //== ***** ***** ***** Component Return ***** ***** ***** ==//
   return (
     <div className="pb-3">
+      {/* START OF "CHOOSE A SUBSCRIPTION" */}
+      {AccountContext.clickwrapIsActive && (
+        <div className="flex w-full flex-col items-center justify-center rounded-lg bg-emerald-200 px-3 py-3 dark:bg-emerald-400">
+          <div className="max-w-md">
+            <p className="text-center font-semibold text-zinc-700 dark:text-emerald-900">
+              All user agreements are active
+            </p>
+            <button>choose a subscription</button>
+          </div>
+        </div>
+      )}
+      {/* END OF "CHOOSE A SUBSCRIPTION" */}
       {/* START OF "USER AGREEMENTS REQUIRED" COMPONENT */}
       {AccountContext.clickwrapStatusFetched &&
         !AccountContext.clickwrapIsActive && (
-          <div className="flex max-w-lg flex-col items-center justify-center rounded-lg bg-emerald-200 px-3 py-3 dark:bg-emerald-400">
+          <div className="flex w-full flex-col items-center justify-center rounded-lg bg-emerald-200 px-3 py-3 dark:bg-emerald-400">
             <div className="max-w-md">
               <p className="text-center font-semibold text-zinc-700 dark:text-emerald-900">
                 The user agreements below are required before previews or
